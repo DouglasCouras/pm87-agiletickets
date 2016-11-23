9ba40ae1478377392df67cbfecbcb78f3fe99f56
Hainison Dornelas <hainison@gmail.com>
2016-11-23 14:31:27 -0200

    corrigindo bug de numero de ingressos de igual ao numero de vagas

--- src/test/java/br/com/caelum/agiletickets/FirstTest.java
+++ src/test/java/br/com/caelum/agiletickets/FirstTest.java
@@ -1,8 +1,11 @@
 package br.com.caelum.agiletickets;
 
 import static org.junit.Assert.assertTrue;
+import junit.framework.Assert;
 
 import org.junit.Test;
+
+import br.com.caelum.agiletickets.models.Sessao;
 
 public class FirstTest {
 
